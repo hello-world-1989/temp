ecd8391da834c7d234db156ce3c9a6f09bc58006
program think backup <103631137+hello-world-1989@users.noreply.github.com>
2025-09-22 13:00:11 +1200

    Update get-apple-id.js

--- src/get-apple-id.js
+++ src/get-apple-id.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-const APPLE_ID_URL= process.env.APPLE_ID_URL;
+const APPLE_ID_URL = process.env.APPLE_ID_URL;
 
 export async function getAppleId() {
   const res = await axios.get(APPLE_ID_URL);
 
   const resData = res.data;
+
+  console.log('resData: ', resData);
   //   const passwordRegex = /密码：(.*?)</;
   let username = resData?.[0]?.username;
   let password = resData?.[0]?.password;
@@ -13,9 +15,8 @@ export async function getAppleId() {
 
   console.log('Success:', username, password, expireDate);
 
-  return [username, password, expireDate]
+  return [username, password, expireDate];
   //   console.log('Success:', htmlString);
 }
 
 // getAppleId();
-
