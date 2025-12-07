6ce50fc5f6230ea422b3a456a3b826a221eab6c2
program think backup <103631137+hello-world-1989@users.noreply.github.com>
2025-12-07 21:21:25 +1300

    Update install.sh

--- docker-xrayr/install.sh
+++ docker-xrayr/install.sh
@@ -31,9 +31,9 @@ cd end-gfw
 
 sudo apt install unzip -y
 sudo apt install nginx -y
-wget https://github.com/hello-world-1989/temp/raw/stable/docker-xrayr/config.zip
-wget https://github.com/hello-world-1989/temp/raw/stable/docker-xrayr/docker-compose.yml
-wget https://github.com/hello-world-1989/temp/raw/stable/docker-web-v2/custom.zip
+wget https://github.com/hello-world-1989/temp/raw/main/docker-xrayr/config.zip
+wget https://github.com/hello-world-1989/temp/raw/main/docker-xrayr/docker-compose.yml
+wget https://github.com/hello-world-1989/temp/raw/main/docker-web-v2/custom.zip
 
 wget https://github.com/hello-world-1989/temp/raw/stable/docker-xrayr/end-gfw.com
 
