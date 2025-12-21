365b1a0c73ae8a7bdbef168cbcd96e6fa5ec12f1
program think backup <103631137+hello-world-1989@users.noreply.github.com>
2025-12-22 10:09:02 +1300

    Update install.sh

--- docker-xrayr/install.sh
+++ docker-xrayr/install.sh
@@ -7,9 +7,9 @@ sudo apt-get install \
     curl \
     gnupg-agent \
     software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 yes '' | sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
